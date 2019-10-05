8cb71a6e828f223793f4442fed27848a2031ad9b
chef27615 <wei.du.2015@marshall.usc.edu>
2019-10-05 00:17:11 -0400

    setup get and post

--- index.js
+++ index.js
@@ -11,9 +11,42 @@ const genres = [
 ]
 
 app.get('/', (req, res) => {
+    res.status(200).json({'connection':'great'})
+})
+
+
+app.get('/api/genres', (req, res) => {
     res.send(genres)
 })
 
+app.get('/api/genres/:id', (req, res) => {
+    const genre = genres.find(g => g.id === parseInt(req.params.id))
+    if(!genre) return res.status(400).json({'Error':'nothing found'})
+    res.send(genre)
+})
+
+app.post('/api/genres', (req, res) => {
+
+    const { error } = validateGenre(req.body);
+    if(error) return res.status(400).send(error.details[0].message)
+
+    const genre = {
+        id : genres.length + 1,
+        name : req.body.name
+    };
+    genres.push(genre)
+    res.status(201).json(genre)
+})
+
+
+
+// help me to validate input
+const validateGenre = (genre) => {
+    const schema ={
+        name : Joi.string().min(4).required()
+    }
+    return Joi.validate(genre, schema)
+}
 
 
 
