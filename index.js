c347c30645a4267f7ad41be0a0df01ef43661e95
chef27615 <wei.du.2015@marshall.usc.edu>
2019-10-09 15:20:37 -0400

    delete unneccessary handler in the index.js file

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 // dependencies
 const express = require('express');
 
-// routes
+// imported routes
 const genres = require('./routes/genres');
 const home = require('./routes/home');
 
 const app = express();
 app.use(express.json());
+
+// routes in app ******
 app.use('/api/genres', genres);
 app.use('/', home);
 
-app.get('/', (req, res) => {
-    res.status(200).json({'connection':'great'})
-})
-
+// ******
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`**********\n\nserver start at ${port}\n\n*********`));
