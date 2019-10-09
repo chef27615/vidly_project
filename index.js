fd88c537d1be213ba2cb8525e6150151c075c2ef
chef27615 <wei.du.2015@marshall.usc.edu>
2019-10-09 15:16:13 -0400

    re-routed home responses

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const express = require('express');
 
 // routes
 const genres = require('./routes/genres');
-
+const home = require('./routes/home');
 
 const app = express();
 app.use(express.json());
 app.use('/api/genres', genres);
-
+app.use('/', home);
 
 app.get('/', (req, res) => {
     res.status(200).json({'connection':'great'})
